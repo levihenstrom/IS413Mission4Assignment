7e5afe26606df90940d54aaa2332370df48cbca1
Levihenny <levi.henstrom@gmail.com>
2026-01-26 13:52:46 -0700

    changes

---
+++ GameTools.cs
@@ -0,0 +1,25 @@
+namespace Mission4Assignment;
+
+public class GameTools
+{
+
+    public void PrintBoard(Array board)
+    {
+        
+        
+    }
+    
+    
+    public int CheckWinner(Array board)
+    {
+        int winner = 0;
+        
+        
+        
+        
+        
+        
+        
+        return winner;
+    }
+}
