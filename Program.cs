563988cefe9c1ff1462d3226cbd6b9b934be0fbb
natcastro <67030361+natcastro@users.noreply.github.com>
2026-01-26 14:16:41 -0700

    program.cs updates

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// welcome the user to the name 
 
-Console.WriteLine("Mission 4");
+using Mission4Assignment;
+
+GameTools gt = new GameTools();
+
+Console.WriteLine("Welcome to Tic Tac Toe Game!: ");
+Console.WriteLine("Please press enter to play");
+
+bool playGame = true;
+
+
+while (playGame)
+{
+    string[] gameBoard = new string[9];
+    string openSpaces = "012345678";
+    
+    bool gameOver = false;
+    while (!gameOver)
+    {
+        gt.PrintBoard(gameBoard);
+        Console.Write("Player 1, Enter you choice: ");
+        char player1 = Convert.ToChar(Console.ReadLine());
+        
+        if (player1 == )
+    }
+}
